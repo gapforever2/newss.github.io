753840656f29a81227c545e68abcfceb3f899a7a
gapforever2 <109466410+gapforever2@users.noreply.github.com>
2024-03-21 15:53:01 +0300

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,7 @@ class _app {
   backgroundToggler = false;
   shouldIgnoreVideo = false;
   effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
-  brandDescription = ['11 years old kids team', 'pro roblox players', 'xane ddosers', 'trashtalkers', 'esoterik fanboys', 'hack vs hack team'];
+  brandDescription = ['Поддержи проект', 'Будь зайкой :3', 'Мы хотим развивать GAF не в вместо вас , а с вами :)', 'Зови друзей , вместе веселей!', 'Все деньги пойдут на развитие сервера.'];
 
   titleChanger = (text, delay) => {
     if (!text) return;
